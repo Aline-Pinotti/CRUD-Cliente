ba7cc71dd999d58db20aa3f111c7b091949161aa
user.name: Aline-Pinotti <user.email: alinefpinotti@gmail.com>
2025-02-16 18:58:44 -0300

    finalize

--- src/main/java/com/devsuperior/DSClient/entities/Client.java
+++ src/main/java/com/devsuperior/DSClient/entities/Client.java
@@ -3,7 +3,6 @@ package com.devsuperior.DSClient.entities;
 import java.time.LocalDate;
 import java.util.Objects;
 
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -18,7 +17,7 @@ public class Client {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
-    @Column(unique = true)
+    // @Column(unique = true)
     private String cpf;
     private Double income;
     private LocalDate birthDate;
--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,4 +1,4 @@
-INSERT INTO tb_client(name, cpf, income, birth_date, children) VALUES ('Maria Brown', '12345678901', 4000.0, '1990-01-01', 1);
+INSERT INTO tb_client(name, cpf, income, birth_date, children) VALUES ('Maria Brown', '32345678901', 4000.0, '1990-01-01', 1);
 INSERT INTO tb_client(name, cpf, income, birth_date, children) VALUES ('Alex Green', '32165498766', 5000.0, '1985-01-01', 2);
 INSERT INTO tb_client(name, cpf, income, birth_date, children) VALUES ('Bob Grey', '65498732145', 3000.0, '1995-01-01', 0);
 INSERT INTO tb_client(name, cpf, income, birth_date, children) VALUES ('Anna White', '98765432102', 6000.0, '1980-01-01', 3);
